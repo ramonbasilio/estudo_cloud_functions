3e547c4c4b98baa5ce1bebc42ae863d14f92bfe1
ramon <ramon.basilio@hotmail.com>
2024-01-15 17:38:59 -0300

    commit inicial

---
+++ functions/index.js
@@ -0,0 +1,8 @@
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+
+exports.helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+
+  response.send("Hello from Firebase!");
+});
